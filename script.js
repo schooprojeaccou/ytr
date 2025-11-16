d0cd91ee2f7285889d8645e84ce25a5f9f0f9a3f
schooprojeaccou <schooprojeaccou@gmail.com>
2025-11-16 12:58:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const presentationData = [
-
   {
-    section: ["Reconciliation and Indigenous Populations of Canada", "", "", "", "", "", ""],
-    parts: []
+    "section": [
+      "Reconciliation and Indigenous Populations of Canada",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ],
+    "parts": []
   },
   {
-    section: ["What is reconciliation?", "", "", "", "", "", ""],
-    parts: [
+    "section": [
+      "What is reconciliation?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
       [
-        "Reconciliation is the process by which one reaches an agreeable resolution for all victim parties following their wrongdoings. In the case of the Indigenous populations of Canada, this involves recognizing, understanding and resolving the past and grave mistreatments of the Indigenous peoples.",
+        "Reconciliation is the process by which one can ‘make up’ for their mistakes. In the case of the Indigenous populations of Canada, this involves recognizing, understanding and resolving the past and grave mistreatments of the Indigenous peoples.",
         "",
         "",
         "",
@@ -17,7 +32,7 @@ const presentationData = [
         ""
       ],
       [
-        "Examples of reconciliation towards Indigenous peoples include, but are not limited to, protesting for, studying of, and supporting the Indigenous peoples and their cultures. These forms of reconciliation are not the only ones, nor are they alone sufficient, but it is still tremendously important and necessary to perform them, even if it is in a minor way. After all, if a sole man sloths due to perceived insignificance, all men shall sloth due to perceived insignificance.",
+        "Examples of reconciliation towards Indigenous peoples include, but are not limited to, protesting for, studying of, and supporting the Indigenous peoples and their cultures. These forms of reconciliation are not the only ones, nor are they alone sufficient, but it is still tremendously important and necessary to perform them, even if it is in a minor way. After all, if even a sole person sloths due to their perceived insignificance, all others shall sloth too due to their perceived insignificance.",
         "",
         "",
         "",
@@ -28,13 +43,359 @@ const presentationData = [
     ]
   },
   {
-    section: ["Credits", "", "", "", "", "", ""],
-    parts: [
-      ["sigma men", "mog.png", "mog.png", "mog.png", "mog.png", "mog.png", "mog.png"],
-      ["ohio men", "", "", "", "", "", ""]
+    "section": [
+      "Who are the Indigenous peoples of Canada?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "In Canada’s history, there were three main populations of Indigenous peoples: Métis, Inuit and the First Nations.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "It is important to note that they all have different histories and cultures, and categorizing them as one people can be harmful.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
     ]
+  },
+  {
+    "section": [
+      "What is the history of the Indigenous peoples of Canada?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "Before the arrival of the Europeans in the Americas, and hence Canada, the Indigenous peoples had lived on this land for thousands of years. For sustenance, the nomads hunted, gathered and fished, whilst the sedentarists engaged in agriculture. They fought wars, inherited culture and deepened their beliefs.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "They lived in groups, communities, clans and nations. They lived in families. There are six main Indigenous geographical groups. They are the Woodland First Nations, the Iroquoian First Nations, the Plains First Nations, the Plateau First Nations, the Pacific Coast First Nations, the First Nations of the Mackenzie and the Yukon River Basins.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "What are some tools, technologies and crafts of the Indigenous peoples?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "The Indigenous peoples of Canada have produced many technologies, be it practical, for ease of life or otherwise. An example of one such technology is the Inuit’s snow goggles made of bone antler, allowing them to help protect their eyes from the reflections of the sun on the snow.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Ultimately, their innovations have left a vital impact on the progression of modern day technology, yet it is not even a bit appreciated.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "What discriminations do the Indigenous peoples face?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "The Indigenous peoples have faced many, many discriminations. Even today, they receive less health care, less governmental financial aid, have higher unemployment rates, and have had their native lands stolen from them.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "They were treated as subhuman. They were treated as savage. They were forced into residential schools. They were victims.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "What exactly are residential schools?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "Residential schools were a culturicide that lasted from the 18th century to the ends of the 20th century (~1700–~1990). Demanded by the Church, the Canadian government at the time hosted these institutions under the guise of “civilizing” the “uncivilized”. In truth, it was merely an evangelist forcing of religion and culture.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "They forced Indigenous children to attend the schools, stripped them of their identity, and placed them in the schools to force them to discard their language, tradition, culture, religion, identity and self and replace it with their own. This forceful, wasteful, hateful, harmful, vile and treacherous acculturation has caused immense trauma, mental and physical, to be dealt to the children.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Hiding the dead bodies and records as if it were not shameful, the combined death toll of the schools is not at all known.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "What has already been done for Indigenous peoples?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "As a form of reconciliation against their wrong-doings, discriminations and hatefulnesses, the Canadian government officially recognized its fault and created the 94 calls to action to aid the Indigenous peoples, alongside the Truth and Reconciliation Commission of Canada.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Additionally, Canada now has a national day, Orange Shirt Day, dedicated solely to the people who had endured the pain and trauma of Canada’s unforgivable crimes. Continually placing attention on Indigenous peoples’ sufferings, it is a valid form of reconciliation.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "What can you do?",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "The 94 calls to actions are not yet completed. Indigenous peoples still face discrimination. Do not fall into the belief that any actions you perform have little to no impact. Yes, perhaps one person alone cannot do anything by their own will. However, one person alone can call onto others. And they can call unto more. Yes, surely a group of people sharing one will can have an impact.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Do not ignore the hate.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Do not ignore the discrimination.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Do not ignore the Indigenous peoples.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Protest.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Garner attention.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Help the Indigenous peoples of Canada.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Help the world.",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "Credits",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "HTML & CSS designing of the site - Hossein with slight use of AI\nJavaScript functionality of the site - Hossein\nSource Searching - Alborz with slight emotional support from Hossein\nRetrieving Informations - Hossein & Alborz\nRetrieving Images - Hossein & Alborz\nWriting - Hossein & Alborz\nImproving and reformatting writing - Hossein\nMeowing - Mr. Kitty Whiskers",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "References",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ],
+    "parts": [
+      [
+        "Government of Canada. (2024-06-13). Indigenous peoples and communities\nRetrieved from https://www.rcaanc-cirnac.gc.ca/eng/1100100013785/1529102490303\nGovernment of Canada. (2017-05-02). First Nations in Canada\nRetrieved from https://www.rcaanc-cirnac.gc.ca/eng/1307460755710/1536862806124\nGovernment of Canada. (2025-05-29). Indigenous history in Canada\nRetrieved from https://www.rcaanc-cirnac.gc.ca/eng/1100100013778/1607903934135",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Government of Canada. (2025-09-29). The Residential School System National Historic Event\nRetrieved from https://parks.canada.ca/culture/designation/evenement-event/sys-pensionnats-residential-school-sys\nINDIGENOUS CORPORATE TRAINING inc. (2022-12-05). 8 Key Issues for Indigenous Peoples in Canada\nRetrieved from: https://www.ictinc.ca/blog/8-key-issues-for-indigenous-peoples-in-canada\nGovernment of Canada. (2025-09-17). Delivering on Truth and Reconciliation Commission Calls to Action\nRetrieved from https://www.rcaanc-cirnac.gc.ca/eng/1524494530110/1557511412801",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ],
+      [
+        "Pawanka Fund. (2023). Indigenous Innovations Use of Indigenous Technologies\nRetrieved from https://pawankafund.org/blog-news/indigenous-innovations-use-of-indigenous-technologies/",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+    ]
+  },
+  {
+    "section": [
+      "Image Sources",
+      "",
+      "",
+      "",
+      "",
+      "",
+      ""
+    ],
+    "parts": []
   }
-
 ];
 
 let totalSpots = -1; 
